4000cbbae6ed01336e87e0f1465bd1fae86b6de5
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-19 19:29:59 +0530

    if else statement

---
+++ flutter_application_1/dart_codes/conditions.dart
@@ -0,0 +1,15 @@
+void main() {
+  var isLoggedIn = false;
+  var userName = "MAx";
+  var password = "tester";
+  var age = 30;
+  if (userName == 'MAx' && password =='tester'){
+    print("LoggedIn");
+  }else if(isLoggedIn){
+    print("overRuled");
+    
+  } 
+  else {
+    print('failded');
+  }
+}
