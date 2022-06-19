d312a9e5a281a780eab2ef52640e06d36378644b
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-19 20:36:55 +0530

    answering 3 question error solved using if else

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -12,38 +12,43 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['black', 'green', 'white', 'yellow']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Horse', 'Cow']
+    },
+    {
+      'questionText': 'What\'s your favorite actor?',
+      'answers': ['Hrithik', 'kartik', 'ranveer', 'Ranbir']
+    },
+  ];
   var questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
       questionIndex = questionIndex + 1;
     });
+    if (questionIndex < questions.length) {
+      print("we have more questions");
+    } else {
+      print("none");
+    }
     // print('Answer Choosen');
-    print(questionIndex);
+    // print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['black', 'green', 'white', 'yellow']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Horse', 'Cow']
-      },
-      {
-        'questionText': 'What\'s your favorite actor?',
-        'answers': ['Hrithik', 'kartik', 'ranveer', 'Ranbir']
-      },
-    ];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
               title: Text('My first app'),
             ),
-            body: Column(children: [
+            body:questionIndex < questions.length ? Column(children: [
               Question(
                 questions[questionIndex]['questionText'] as String,
               ),
@@ -54,6 +59,9 @@ class _MyAppState extends State<MyApp> {
               // Answer(answerQuestion),
               // Answer(answerQuestion),
               // Answer(answerQuestion),
-            ])));
+            ]
+            ):Center(child:Text("Done"),
+            )
+            ));
   }
 }
