f9e27b4eebf156a4012c95d4c81e1073f646a8a7
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-20 12:07:58 +0530

    basic quiz app in flutter

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,67 +1,179 @@
-import 'package:flutter/material.dart';
-import './question.dart';
-import 'answer.dart';
+// import 'package:flutter/material.dart';
+// import './question.dart';
+// import 'answer.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   final questions = const [
+//     {
+//       'questionText': 'What\'s your favorite color?',
+//       'answers': [
+//         {'text': 'Black' , 'score': 10},
+//         {'text': 'Green', 'score': 5},
+//         {'text': 'Blue', 'score': 2},
+//         {'text': 'Red', 'score': 1},  
+//       ]
+//     },
+//     {
+//       'questionText': 'What\'s your favorite animal?',
+//       'answers': [{'text': 'Dog' , 'score': 10},
+//         {'text': 'cat', 'score': 5},
+//         {'text': 'horse', 'score': 2},
+//         {'text': 'cow', 'score': 1}, ]
+//     },
+//     {
+//       'questionText': 'What\'s your favorite actor?',
+//       'answers': [{'text': 'Hrithik' , 'score': 10},
+//         {'text': 'Srk', 'score': 5},
+//         {'text': 'tiger', 'score': 2},
+//         {'text': 'Ntr', 'score': 1}, ]
+//     },
+//   ];
+//   var questionIndex = 0;
+
+//   void answerQuestion(int score) {
+//     setState(() {
+//       questionIndex = questionIndex + 1;
+//     });
+//     if (questionIndex < questions.length) {
+//       print("we have more questions");
+//     } else {
+//       print("none");
+//     }
+//     // print('Answer Choosen');
+//     // print(questionIndex);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//             appBar: AppBar(
+//               title: Text('My first app'),
+//             ),
+//             body: questionIndex < questions.length
+//                 ? Column(children: [
+//                     Question(
+//                       questions[questionIndex]['questionText'] as String,
+//                     ),
+//                     ...(questions[questionIndex]['answers'] as List<Map<String , Object>>)
+//                         .map((answer) {
+//                       return Answer( () => answerQuestion(answer['score']) , answer['text']);
+//                     }).toList()
+//                     // Answer(answerQuestion),
+//                     // Answer(answerQuestion),
+//                     // Answer(answerQuestion),
+//                   ])
+//                 : Center(
+//                     child: Text("Done"),
+//                   )));
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['black', 'green', 'white', 'yellow']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Dog', 'Cat', 'Horse', 'Cow']
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
-      'questionText': 'What\'s your favorite actor?',
-      'answers': ['Hrithik', 'kartik', 'ranveer', 'Ranbir']
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
-  var questionIndex = 0;
+  var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void answerQuestion() {
+  void _resetQuiz() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = 0;
+      _totalScore = 0;
     });
-    if (questionIndex < questions.length) {
-      print("we have more questions");
+  }
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
     } else {
-      print("none");
+      print('No more questions!');
     }
-    // print('Answer Choosen');
-    // print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My first app'),
-            ),
-            body:questionIndex < questions.length ? Column(children: [
-              Question(
-                questions[questionIndex]['questionText'] as String,
-              ),
-              ...(questions[questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                return Answer(answerQuestion, answer);
-              }).toList()
-              // Answer(answerQuestion),
-              // Answer(answerQuestion),
-              // Answer(answerQuestion),
-            ]
-            ):Center(child:Text("Done"),
-            )
-            ));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
   }
 }
