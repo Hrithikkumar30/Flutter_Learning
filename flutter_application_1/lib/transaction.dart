bf1e909fe00232ed71f62aaa9d02be0180cb46aa
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-22 11:03:53 +0530

    transaction class created

---
+++ flutter_application_1/lib/transaction.dart
@@ -0,0 +1,13 @@
+class Transactin {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transactin({
+    this.id, 
+    this.title, 
+    this.amount, 
+    this.date
+    });
+}
