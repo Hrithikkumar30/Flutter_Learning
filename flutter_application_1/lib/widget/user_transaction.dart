fdb09702a8025a698df7b51e7aa731c513cca7f9
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-25 20:42:11 +0530

    transaction list feature added with scroll option

--- flutter_application_1/lib/widget/user_transaction.dart
+++ flutter_application_1/lib/widget/user_transaction.dart
@@ -30,6 +30,6 @@ class _UserTransactionState extends State<UserTransaction> {
   }
   @override
   Widget build(BuildContext context) {
-    return Column(children: [NewTransaction(), TransactionList(_userTransactions)]);
+    return Column(children: [NewTransaction(_addNewTransaction), TransactionList(_userTransactions)]);
   }
 }
